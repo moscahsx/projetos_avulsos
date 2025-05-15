113f26e789f669ab4760366f833a0da7a4e73fe4
Mosquito <pedromonteirofontes1@gmail.com>
2025-05-15 13:14:13 -0300

    atualização

--- projetos_javas/script.js
+++ projetos_javas/script.js
@@ -26,3 +26,55 @@ function somar(){
 
   alert(multiplicar);
 }
+var x = 0 ;
+
+  document.write("Iniciando o Loop...</br>");
+ 
+while(x < 10){
+
+  document.write("Numero: "+x+"</br>");
+
+  x++;
+
+}
+document.write("Finalizando o Loop...</br>");
+
+for( x = 0; x<10 ; x++ ){
+  document.write("Número "+x+"</br>");
+}
+y = prompt("Qual é o Número ?");
+
+switch(y){
+  case "0" :
+    alert("Y é zero ");
+      break;
+    case "1" :
+      alert("Y é Um");
+        break;
+        case "2":
+          alert("Y é dois");
+          break;
+          default:
+            alert("Ninguém foi satisfeiro!");
+            break;
+}
+
+function verificar(){
+  var n1 = document.getElementById("n1").innerHTML;
+  var n2 = document.getElementById("n2").value;
+
+  if(n1 == n2){
+    alert("você acertou o número")
+  } else {
+    alert("você errou o número")
+  }
+
+  reset();
+}
+function reset() {
+  document.getElementById ("n2").value = "";
+
+  var r = Math.floor(Math.random()*100);
+  document.getElementById("n1").innerHTML = r;
+  
+}
